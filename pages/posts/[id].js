e2977a024c0fb8d20b3c30f0a689d4143c9edb86
Philipp Weiss <p.weiss85@gmx.de>
2022-10-05 17:36:09 +0200

    add ssg for posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Link from "next/link";
+import { getPostById } from "../../services/postService";
+import { getAllPosts } from "../../services/postService";
 
 /*
  * Make all necessary imports.
@@ -8,16 +11,39 @@ import Head from "next/head";
  * Render the data.
  */
 
-export default function Post() {
+export async function getStaticPaths() {
+  const posts = await getAllPosts();
+  const ids = posts.map((post) => post.id);
+
+  return {
+    paths: ids.map((id) => ({ params: { id: id } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const posts = await getPostById(id);
+
+  return {
+    props: { name: posts.name, description: posts.description, id: posts.id },
+  };
+}
+
+export default function Post({ name, description, id }) {
   return (
     <>
       <Head>
         <title>title</title>
       </Head>
-      <h1>name of the post</h1>
-      <p>ID:</p>
-      <h2>Description</h2>
-      <p></p>
+      <h1>{name} of the post</h1>
+      <p>ID: {id}</p>
+      <h2>{description}</h2>
+      <Link href={`/posts`} passHref>
+        <a>
+          <p>Back to Posts</p>
+        </a>
+      </Link>
     </>
   );
 }
